c29def2952240c3712f1ccf28da85543ac829c5b
Ellie Carpenter <espanswick@gmail.com>
2025-01-23 15:27:37 +0000

    Update views to show approved reviews without login

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,4 @@
+
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView
 from django.urls import reverse, reverse_lazy
@@ -5,87 +6,70 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import HttpResponseForbidden, HttpResponseRedirect
-from django.utils.decorators import method_decorator
-
+from django.http import HttpResponseRedirect
 from .models import Review
 from .forms import ReviewForm
 
-
-# ListView to display reviews
 class ReviewListView(ListView):
     model = Review
     template_name = 'review_list.html'
     context_object_name = 'reviews'
+    
+def review_list(request):
+    if request.user.is_authenticated:
+        # Display approved reviews and pending reviews by the logged-in user
+        reviews = Review.objects.filter(approved=True) | Review.objects.filter(author=request.user)
+    else:
+        reviews = Review.objects.filter(approved=True)
 
-    def get_queryset(self):
-        """
-        Show approved reviews or the user's reviews.
-        """
-        if self.request.user.is_authenticated:
-            return Review.objects.filter(approved=True) | Review.objects.filter(author=self.request.user)
-        return Review.objects.filter(approved=True)
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
 
+
+
+class ReviewCreateView(CreateView):
+    model = Review
+    fields = ['review_title', 'content', 'rating']
+    template_name = 'add_review.html'
+    success_url = reverse_lazy('review')
 
 @login_required
 def add_review(request):
-    """
-    Function-based view to add a new review.
-    """
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
             review.author = request.user
             review.save()
-            messages.success(request, "Review added! It will be reviewed for approval.")
             return redirect('review')
     else:
         form = ReviewForm()
 
     return render(request, 'reviews/add_review.html', {'form': form})
 
-
-# Restricting access to the update view to the author
-@method_decorator(login_required, name='dispatch')
 class ReviewUpdateView(UpdateView):
     model = Review
     fields = ['review_title', 'content', 'rating']
     template_name = 'add_review.html'
+    context_object_name = 'review'
     success_url = reverse_lazy('review')
 
-    def dispatch(self, request, *args, **kwargs):
-        review = self.get_object()
-        # Ensure the logged-in user is the author
-        if review.author != request.user:
-            return HttpResponseForbidden("You are not allowed to edit this review.")
-        return super().dispatch(request, *args, **kwargs)
-
     def form_valid(self, form):
+        # Reset the approval status when the review is edited
         review = form.save(commit=False)
-        # Reset the approval status when edited
         review.approved = False
         review.save()
-        messages.success(self.request, "Review updated and sent for approval!")
+        messages.add_message(self.request, messages.SUCCESS, 'Review updated and sent for approval!')
         return super().form_valid(form)
-
 
 @login_required
 def edit_review(request, review_id):
-    """
-    Function-based view to edit a review.
-    """
     review = get_object_or_404(Review, pk=review_id)
-
-    # Restrict access to the review's author
-    if review.author != request.user:
-        return HttpResponseForbidden("You are not allowed to edit this review.")
 
     if request.method == "POST":
         review_form = ReviewForm(data=request.POST, instance=review)
         if review_form.is_valid():
             updated_review = review_form.save(commit=False)
-            # Reset approval status when edited
+            # Reset approval status when the review is edited
             updated_review.approved = False
             updated_review.save()
             messages.success(request, "Review updated! It will be re-evaluated for approval.")
@@ -99,32 +83,14 @@ def edit_review(request, review_id):
     return render(request, 'reviews/edit_review.html', {'form': review_form, 'review': review})
 
 
-# Restricting access to the delete view to the author
-@method_decorator(login_required, name='dispatch')
+
+
 class ReviewDeleteView(SuccessMessageMixin, DeleteView):
     model = Review
     template_name = 'reviews/review_confirm_delete.html'
     success_url = reverse_lazy('review')  # Redirect to the review list page
     success_message = "Review deleted successfully!"
 
-    def dispatch(self, request, *args, **kwargs):
-        review = self.get_object()
-        # Ensure the logged-in user is the author
-        if review.author != request.user:
-            return HttpResponseForbidden("You are not allowed to delete this review.")
-        return super().dispatch(request, *args, **kwargs)
-
-
-# Filtering reviews by the logged-in user
 @login_required
-def review_list(request):
-    """
-    Custom review list function to filter reviews for logged-in users.
-    """
-    if request.user.is_authenticated:
-        # Display approved reviews and pending reviews by the logged-in user
-        reviews = Review.objects.filter(approved=True) | Review.objects.filter(author=request.user)
-    else:
-        reviews = Review.objects.filter(approved=True)
-
-    return render(request, 'reviews/review_list.html', {'reviews': reviews})
+def get_queryset(self):
+        return super().get_queryset().filter(author=self.request.user)
