0658ed11fff3b1c391e6697466d3b5bdf6fd7412
Ellie Carpenter <espanswick@gmail.com>
2025-01-27 14:07:48 +0000

    Fix issue with comment and review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,8 +5,22 @@ from .models import Review
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
-        'author', 'review_title', 'rating', 'created_on', 'approved')
-    list_filter = ('author', 'rating', 'created_on')
+        'author',
+        'reviewer_name',
+        'review_title',
+        'rating',
+        'created_on',
+        'approved',
+    )
+    list_filter = (
+        'author',
+        'rating',
+        'created_on',
+        'approved',
+    )
     search_fields = (
-        'author', 'review_title', 'user__username', 'content'
+        'author__username',
+        'review_title',
+        'reviewer_name',
+        'content',
     )
