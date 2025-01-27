f03ddfb460523fe70a64d6cf2e6cb4beb051e035
Ellie Carpenter <espanswick@gmail.com>
2025-01-27 16:14:27 +0000

    Update contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from contact.models import ContactRequest
 
-admin.site.register(ContactRequest)
+@admin.register(ContactRequest)
+class ContactRequestAdmin(admin.ModelAdmin):  # Ensure it's admin.ModelAdmin
+    list_display = ('name', 'email', 'subject', 'read')  
+    search_fields = ('name', 'email', 'subject')  
+    list_filter = ('read',)
