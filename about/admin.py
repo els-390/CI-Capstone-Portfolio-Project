de5fb9eed2a573f284ef7991127afb69dfb22c9a
Ellie Carpenter <espanswick@gmail.com>
2025-01-27 16:11:11 +0000

    Add contact request to About admin

--- about/admin.py
+++ about/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
-from .models import About
-
+from .models import About, ContactRequest
 from django_summernote.admin import SummernoteModelAdmin
-
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+
+@admin.register(ContactRequest)
+class ContactRequestAdmin(admin.ModelAdmin):  # Ensure it's admin.ModelAdmin
+    list_display = ('name', 'email', 'subject', 'read')  
+    search_fields = ('name', 'email', 'subject')  
+    list_filter = ('read',)
